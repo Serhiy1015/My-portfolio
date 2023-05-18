45deb3e767476fe450ee2ed5d4c43e2a3e8d6713
Serhiy Karpiak <121126967+Serhiy1015@users.noreply.github.com>
2023-05-18 11:25:01 +0300

    Add files via upload
    Завершив роботу над проектом

---
+++ lang.js
@@ -0,0 +1,74 @@
+const langArr = {
+    'my-name': {
+        'ua': 'Сергій Карпяк',
+        'eng': 'Serhiy Karpiak'
+    },
+    'stack': {
+        'ua': 'Стек:',
+        'eng': 'Stack:'
+    },
+    'librares': {
+        'ua': 'Бібліотеки:',
+        'eng': 'Librares:'
+    },
+    'atm': {
+        'ua': 'В процесі вивчення: React',
+        'eng': 'Studying right now: React'
+    },
+    'my-projects-h1': {
+        'ua': 'Мої проєкти',
+        'eng': 'My projects'
+    },
+    'click-p': {
+        'ua': '(Натисніть на блок для переходу на сайт)',
+        'eng': '(Click on the block to go to the website)'
+    },
+    'tatto-h3': {
+        'ua': 'Лендінг для тату-студії',
+        'eng': 'Landing page for a tattoo studio'
+    },
+    'tatto-p': {
+        'ua': 'Технології: HTML, CSS, JavaScript, AOS.js, Swiper.js <br> Адаптований під будь-які пристрої лендінг з великою кількістю анімації',
+        'eng': 'Technologies: HTML, CSS, JavaScript, AOS.js, Swiper.js <br> Fully responsive landing page with a lot of animations'
+    },
+    'quiz-h3': {
+        'ua': 'JavaScript Квіз',
+        'eng': 'JavaScript Quiz'
+    },
+    'quiz-p': {
+        'ua': 'Технології: HTML, CSS, JavaScript. <br> Одна з найцікавіших моїх робіт. Питання генеруються в рандомному порядку, в кінці гравець бачить кількість правильних відповідей та може пройти знову.',
+        'eng': 'Technologies: HTML, CSS, JavaScript. <br> One of my most interesting projects. Questions are generated in random order, and at the end, the player can see the number of correct answers and restart the quiz.'
+    },
+    'password-h3': {
+        'ua': 'Генератор паролів',
+        'eng': 'Password generator'
+    },
+    'password-p': {
+        'ua': 'Технології: HTML, SCSS, JavaScript. <br> Генератор дає опції на вибір щодо довжини та вмісту паролю. Пароль копіюється при кліку на нього.',
+        'eng': 'Technologies: HTML, SCSS, JavaScript. <br> The generator provides options to choose the length and content of the password. The password is copied when clicked on.'
+    },
+    'building-h3': {
+        'ua': 'Лендінг для будівельної компанії',
+        'eng': 'Building company landing page'
+    },
+    'building-p': {
+        'ua': 'Технології: HTML, CSS, JavaScript. <br> Одна з найперших та найпростіших моїх робіт. Функціонал: випадаюче меню і бургер-меню. Також цікаво було стилізувати скрол. Сайт адаптований під усі пристрої',
+        'eng': 'Technologies: HTML, CSS, JavaScript. <br> One of my earliest and simplest projects. Functionality includes a dropdown menu and a burger menu. It was also interesting to style the scroll. The website is responsive and adapts to all devices.'
+    },
+    'notes-h3': {
+        'ua': 'Нотатки',
+        'eng': 'Notes'
+    },
+    'notes-p': {
+        'ua': 'Технології: HTML, CSS, JavaScript. <br> Тут можна додавати та видаляти нотатки.',
+        'eng': 'Technologies: HTML, CSS, JavaScript. <br> Here you can add and delete notes.'
+    },
+    'stopwatch-h3': {
+        'ua': 'Секундомір',
+        'eng': 'Stopwatch'
+    },
+    'stopwatch-p': {
+        'ua': 'Технології: HTML, CSS, JavaScript. <br> Є можливість зупиняти, відновлювати та скидати час. Над дизайном сильно не заморочувався, перевагу віддав функціоналу.',
+        'eng': 'Technologies: HTML, CSS, JavaScript. <br> There is the ability to pause, resume, and reset the timer. The design is kept simple with more emphasis on functionality.'
+    }
+}
---
+++ script.js
@@ -0,0 +1,41 @@
+const select = document.querySelector('select')
+const allLang = ['ua', 'eng']
+
+select.addEventListener('change', changeURLLanguage)
+
+function changeURLLanguage(){
+    let lang = select.value
+    location.href = window.location.pathname + '#' + lang
+    location.reload()
+}
+
+function changeLanguage() {
+    let hash = window.location.hash
+    hash = hash.substr(1)
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#ua'
+        location.reload()
+    }
+    select.value = hash
+
+    document.querySelector('.my-name').innerHTML = langArr['my-name'][hash]
+    document.querySelector('.stack').innerHTML = langArr['stack'][hash]
+    document.querySelector('.librares').innerHTML = langArr['librares'][hash]
+    document.querySelector('.atm').innerHTML = langArr['atm'][hash]
+    document.querySelector('.my-projects-h1').innerHTML = langArr['my-projects-h1'][hash]
+    document.querySelector('.click-p').innerHTML = langArr['click-p'][hash]
+    document.querySelector('.tatto-h3').innerHTML = langArr['tatto-h3'][hash]
+    document.querySelector('.tatto-p').innerHTML = langArr['tatto-p'][hash]
+    document.querySelector('.quiz-h3').innerHTML = langArr['quiz-h3'][hash]
+    document.querySelector('.quiz-p').innerHTML = langArr['quiz-p'][hash]
+    document.querySelector('.password-h3').innerHTML = langArr['password-h3'][hash]
+    document.querySelector('.password-p').innerHTML = langArr['password-p'][hash]
+    document.querySelector('.building-h3').innerHTML = langArr['building-h3'][hash]
+    document.querySelector('.building-p').innerHTML = langArr['building-p'][hash]
+    document.querySelector('.notes-h3').innerHTML = langArr['notes-h3'][hash]
+    document.querySelector('.notes-p').innerHTML = langArr['notes-p'][hash]
+    document.querySelector('.stopwatch-h3').innerHTML = langArr['stopwatch-h3'][hash]
+    document.querySelector('.stopwatch-p').innerHTML = langArr['stopwatch-p'][hash]
+}
+
+changeLanguage()
